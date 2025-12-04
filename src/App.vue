50ca33556faccd57caf79cb8c274a92735f0b57a
irie-yamashita <irie.yamashita.7e8@itb.cat>
2025-12-04 17:25:18 +0100

    add fetch API + show data

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
-<script setup></script>
+<script setup>
+import { useFetch } from '@/composables/useFetch.js';
+import { ref } from 'vue';
+
+
+const cerca = ref("chicken");
+const url = ref(`https://www.themealdb.com/api/json/v1/1/search.php?s=${cerca.value}`);
+
+
+const { data, error, loading, fetchData} = useFetch(url);
+
+console.log(url.value)
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>The Real Meal</h1>
+
+  <div v-if="loading">Carregant...</div>
+  <div v-else-if="error">{{ error }}</div>
+  <div v-else>
+    <ul v-if="data && data.meals">
+      <li v-for="meal in data.meals">{{ meal.strMeal }}</li>
+    </ul>
+  </div>
+
+  
 </template>
 
 <style scoped></style>
---
+++ src/composables/useFetch.js
@@ -0,0 +1,34 @@
+import { onMounted, ref } from "vue";
+
+
+export function useFetch (url) {
+
+    const data = ref(null);
+    const error = ref(null);
+    const loading = ref(false);
+
+    const fetchData = async () => {
+        
+        // reiniciem valors
+        loading.value = true;
+        error.value = null;
+
+        try {
+            const res = await fetch(url.value);
+            if(!res.ok) {
+                throw new Error(`Error en fer la petició: ${res.status}`);
+            }
+            data.value = await res.json();
+
+        } catch (err) {
+            error.value = err.message;
+        } finally {
+            loading.value = false;
+        }
+
+    }
+
+    onMounted(fetchData);
+
+    return { data, error, loading, fetchData} 
+}
